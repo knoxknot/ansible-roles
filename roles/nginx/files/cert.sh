fe49bff537f9296ab222fccdefc5e4de8454fc8b
Samuel Nwoye <nwoyesamuelc@gmail.com>
2020-01-18 11:05:25 +0100

    Updated the cert.sh file

--- roles/nginx/files/cert.sh
+++ roles/nginx/files/cert.sh
@@ -1,1 +1,11 @@
-openssl req -newkey rsa:2048 -nodes -keyout /etc/nginx/ssl/192.168.10.1.key -x509 -days 365 -out /etc/nginx/ssl/192.168.10.1.crt -subj "/C=COUNTRY_CODE/ST=STATE/L=CITY/O=ORGANIZATION/OU=DEPARTMENT/CN=192.168.10.1" 
+!#/bin/bash
+
+echo "Creating Self Signed Certificates"
+
+# get the host IP of the machine
+HOST_IP=`hostname -I | awk '{print $2}'` && echo $HOST_IP
+
+# get the hostname of the machine
+HOSTNAME=$(hostname -s)  && echo $HOSTNAME
+
+openssl req -newkey rsa:2048 -nodes -keyout /etc/nginx/ssl/$HOST_IP.key -x509 -days 365 -out /etc/nginx/ssl/$HOST_IP.crt -subj "/C=COUNTRY_CODE/ST=STATE/L=CITY/O=ORGANIZATION/OU=DEPARTMENT/CN=$HOST_IP,$HOSTNAME"
